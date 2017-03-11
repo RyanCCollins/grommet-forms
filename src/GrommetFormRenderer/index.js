5e4590ae502bf28608c33923d51c1504a7307c9e
Ryan Collins <rccproductions@mac.com>
2017-03-11 16:15:07 -0500

    Feat: import formfield in form renderer

--- src/GrommetFormRenderer/index.js
+++ src/GrommetFormRenderer/index.js
@@ -1,14 +1,13 @@
 // @flow
 import React from 'react';
 import Form from 'grommet/components/Form';
+import FormField from 'grommet/components/FormField';
 import FormFieldMap from '../FormFieldMap';
 import type { FormFieldType } from '../GrommetForm/types';
 
-export default function GrommetFormRenderer(props: {
+export default function GrommetFormRenderer({ fields }: {
   fields: Array<FormFieldType>
 }) {
-  const { fields } = props;
-  
   if (!fields.length) {
     return null;
   }
